b0424eea50aae931e97429cf7d66ea036593a37b
David Vitrant <david@synteny.us>
2014-03-20 10:09:34 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoogleApi::Application.config.secret_token = '0c5a9573ddd56b9d935dd80cb4e30ce4232e957eb3363e65746c33f262c7cc3bbeb2496b34e99e968de74e6b10c427bc2958fc69c8ad15e7b90c5a66d5c0e3f8'
