20c445f34d727060d1c6725c59675b02140bca99
Patrick Kennedy <bigredtriangle@gmail.com>
2014-03-21 14:50:14 -0400

    in-progress checkin

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,11 +1,14 @@
 class MapsController < ApplicationController
+  respond_to :json
+
   def new
+    puts params
   end
 
   def create
     p params
     respond_to do |format|
-      format.js {}
+      format.js
     end
   end
 
